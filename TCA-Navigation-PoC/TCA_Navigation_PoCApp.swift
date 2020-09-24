b03ef8912430506448a3cc91bb4258906626b180
moto0000 <moto0095@gmail.com>
2020-09-24 16:59:21 +0200

    Initial Commit

---
+++ TCA-Navigation-PoC/TCA_Navigation_PoCApp.swift
@@ -0,0 +1,17 @@
+//
+//  TCA_Navigation_PoCApp.swift
+//  TCA-Navigation-PoC
+//
+//  Created by Maciej Kozlowski on 24/09/2020.
+//
+
+import SwiftUI
+
+@main
+struct TCA_Navigation_PoCApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
